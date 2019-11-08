b5449023d62ec312e47e7da009742c08eb0eac64
amrmynd <ariley.amy@gmail.com>
2019-11-08 14:49:40 +0100

    converts microservice into Eureka client

--- src/main/java/com/example/moviecatalogservice/resourses/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resourses/MovieCatalogResource.java
@@ -3,6 +3,8 @@ package com.example.moviecatalogservice.resourses;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,17 +18,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        List<Rating> ratings = Arrays.asList(
-            new Rating("1234", 4),
-            new Rating("5678", 3)
-        );
-
-        return ratings.stream().map(rating -> {
+        return ratings.getUserRating().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
